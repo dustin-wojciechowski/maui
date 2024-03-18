35660a3a55aa3502c02af9d3891455fb1439b5ea
Jakub Florkowski <kubaflo123@gmail.com>
2024-03-18 11:57:35 +0100

    Enable the UI Test on all platforms (#20801)

--- src/Controls/samples/Controls.Sample.UITests/Issues/Issue20801.xaml.cs
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue20801.xaml.cs
@@ -3,7 +3,7 @@ using Microsoft.Maui.Controls;
 
 namespace Maui.Controls.Sample.Issues
 {
-	[Issue(IssueTracker.Github, "20801", "[Android] ImageButton Border does not fit to CornerRadius", PlatformAffected.Android)]
+	[Issue(IssueTracker.Github, "20801", "ImageButton Border does not fit to CornerRadius", PlatformAffected.All)]
 	public partial class Issue20801 : ContentPage
 	{
 		public Issue20801()
