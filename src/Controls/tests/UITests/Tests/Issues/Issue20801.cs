1271499dbe1906bacec5d41325608292eab76374
Jakub Florkowski <kubaflo123@gmail.com>
2024-03-18 01:51:46 +0100

    Added a Ui test (#20801)

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue20801.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues;
+
+public class Issue20801 : _IssuesUITest
+{
+	public override string Issue => "[Android] ImageButton Border does not fit to CornerRadius";
+
+	public Issue20801(TestDevice device)
+		: base(device)
+	{ }
+
+    [Test]
+	public void CornersShouldBeProperlyRounded()
+	{
+		this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Mac, TestDevice.Windows });
+
+		_ = App.WaitForElement("imageButton");
+
+		// The test passes if corners are properly rounded
+		VerifyScreenshot();
+	}
+}
