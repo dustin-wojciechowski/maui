06ff9606651f9eb0d3df2fddbef798dc5aca8916
Jakub Florkowski <kubaflo123@gmail.com>
2024-03-29 01:42:25 +0100

    Added a sample (#19970)

--- src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/iOS/iOSStatusBarPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/PlatformSpecifics/iOS/iOSStatusBarPage.xaml.cs
@@ -12,36 +12,273 @@ namespace Maui.Controls.Sample.Pages
 			InitializeComponent();
 		}
 
-		void OnPrefersStatusBarHiddenButtonClicked(object sender, EventArgs e)
+		void NavigationPage_Clicked(object sender, EventArgs e)
 		{
-			switch (On<iOS>().PrefersStatusBarHidden())
-			{
-				case StatusBarHiddenMode.Default:
-					On<iOS>().SetPrefersStatusBarHidden(StatusBarHiddenMode.True);
-					break;
-				case StatusBarHiddenMode.True:
-					On<iOS>().SetPrefersStatusBarHidden(StatusBarHiddenMode.False);
-					break;
-				case StatusBarHiddenMode.False:
-					On<iOS>().SetPrefersStatusBarHidden(StatusBarHiddenMode.Default);
-					break;
-			}
+			Navigation.PushAsync(new iOSStatusBarPageNavigationPageDemo());
 		}
 
-		void OnPreferredStatusBarUpdateAnimationButtonClicked(object sender, EventArgs e)
+		void TabbedPage_Clicked(object sender, EventArgs e)
 		{
-			switch (On<iOS>().PreferredStatusBarUpdateAnimation())
+			Navigation.PushAsync(new iOSTabbedPageFlyoutPageDemo());
+		}
+
+		void FlyoutPage_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new iOSStatusBarPageFlyoutPageDemo());
+		}
+
+		void Shell_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new iOSShellDemo());
+		}
+	}
+
+	public class iOSStatusBarPageNavigationPageDemo : ContentPage
+	{
+		public iOSStatusBarPageNavigationPageDemo()
+		{
+			Content = new StackLayout()
 			{
-				case UIStatusBarAnimation.None:
-					On<iOS>().SetPreferredStatusBarUpdateAnimation(UIStatusBarAnimation.Fade);
-					break;
-				case UIStatusBarAnimation.Fade:
-					On<iOS>().SetPreferredStatusBarUpdateAnimation(UIStatusBarAnimation.Slide);
-					break;
-				case UIStatusBarAnimation.Slide:
-					On<iOS>().SetPreferredStatusBarUpdateAnimation(UIStatusBarAnimation.None);
-					break;
-			}
+				Margin = new Microsoft.Maui.Thickness(20),
+				Children = {
+					new Button()
+					{
+						Text = "Toggle Home Indicator",
+						Command = new Command(() => On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()))
+					},
+					new Button()
+					{
+						Text = "Navigate back",
+						Command = new Command(() => Navigation.PopAsync())
+					}
+				}
+			};
+		}
+	}
+
+	public class iOSStatusBarPageFlyoutPageDemo : Microsoft.Maui.Controls.FlyoutPage
+	{
+		public iOSStatusBarPageFlyoutPageDemo()
+		{
+			Flyout = new ContentPage()
+			{
+				Title = "Detail",
+				Content = new StackLayout()
+				{
+					Children = {
+						new Button()
+						{
+							Text = "Open page 1",
+							Command = new Command(() => Detail = OpenPage("Page 1"))
+						},
+						new Button()
+						{
+							Text = "Open page 2",
+							Command = new Command(() => Detail = OpenPage("Page 2"))
+						}
+					}
+				}
+			};
+
+			Detail = new ContentPage()
+			{
+				Content = new StackLayout()
+				{
+					Margin = new Microsoft.Maui.Thickness(20),
+					Children = {
+						new Button()
+						{
+							Text = "Click to open the flyout",
+							Command = new Command(() => IsPresented = true)
+						},
+						new Button()
+						{
+							Text = "Navigate back",
+							Command = new Command(() => Navigation.PopAsync())
+						}
+					}
+				}
+			};
+		}
+
+		public ContentPage OpenPage(string title)
+		{
+			return new ContentPage()
+			{
+				Title = title,
+				Content = new StackLayout()
+				{
+					Margin = new Microsoft.Maui.Thickness(20),
+					Children = {
+						new Button
+						{
+							Text = "Toggle Home Indicator",
+							Command = new Command(() => On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()))
+						},
+						new Button()
+						{
+							Text = "Click to open the flyout",
+							Command = new Command(() => IsPresented = true)
+						},
+						new Button
+						{
+							VerticalOptions = LayoutOptions.End,
+							Text = "Back",
+							Command = new Command(() => Navigation.PopAsync())
+						}
+					}
+				}
+			};
+		}
+	}
+
+	public class iOSTabbedPageFlyoutPageDemo : Microsoft.Maui.Controls.TabbedPage
+	{
+		public iOSTabbedPageFlyoutPageDemo()
+		{
+
+			Children.Add
+			(
+				new ContentPage()
+				{
+					Title = "Tab 1",
+					Content = new StackLayout()
+						{
+							Margin = new Microsoft.Maui.Thickness(20),
+							Children =
+							{
+								new Button()
+								{
+									Text = "Toggle Home Indicator",
+									Command = new Command(() => On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()))
+								},
+								new Button()
+								{
+									Text = "Navigate back",
+									Command = new Command(() => Navigation.PopAsync())
+								}
+							}
+						}
+				}
+			);
+
+			Children.Add
+			(
+				new ContentPage()
+				{
+					Title = "Tab 2",
+					Content = new StackLayout()
+					{
+						Margin = new Microsoft.Maui.Thickness(20),
+						Children =
+						{
+							new Button()
+							{
+								Text = "Toggle Home Indicator",
+								Command = new Command(() => On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()))
+							},
+							new Button()
+							{
+								Text = "Navigate back",
+								Command = new Command(() => Navigation.PopAsync())
+							}
+						}
+					}
+				}
+			);
+
+		}
+	}
+
+	public class iOSShellDemo : Shell
+	{
+		public iOSShellDemo()
+		{
+			TabBar tabBar = new TabBar();
+
+			var toggleHomeIndicatorButton1 = new Button()
+			{
+				Text = "Toggle Home Indicator",
+				Command = new Command(() => On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()))
+			};
+
+			var contentPage1 = new ContentPage
+			{
+				Title = "Shell Tab 1",
+				Content = new StackLayout()
+				{
+					Margin = new Microsoft.Maui.Thickness(20),
+					Children =
+					{
+						toggleHomeIndicatorButton1,
+						new Button()
+						{
+							Text = "Navigate back",
+							Command = new Command(() => Navigation.PopAsync())
+						}
+					}
+				}
+			};
+
+			toggleHomeIndicatorButton1.Command = new Command(() => contentPage1.On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()));
+
+			tabBar.Items.Add
+			(
+				new Tab
+				{
+					Title = "Shell Tab 1",
+					Items = {
+						new ShellContent
+						{
+							Content = contentPage1,
+							Title = "Shell Tab 1",
+						}
+					}
+				}
+			);
+
+			var toggleHomeIndicatorButton2 = new Button()
+			{
+				Text = "Toggle Home Indicator",
+				Command = new Command(() => On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()))
+			};
+
+			var contentPage2 = new ContentPage
+			{
+				Title = "Shell Tab 2",
+				Content = new StackLayout()
+				{
+					Margin = new Microsoft.Maui.Thickness(20),
+					Children =
+					{
+						toggleHomeIndicatorButton2,
+						new Button()
+						{
+							Text = "Navigate back",
+							Command = new Command(() => Navigation.PopAsync())
+						}
+					}
+				}
+			};
+
+			toggleHomeIndicatorButton2.Command = new Command(() => contentPage2.On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden()));
+
+			tabBar.Items.Add
+			(
+				new Tab
+				{
+					Title = "Shell Tab 2",
+					Items = {
+						new ShellContent
+						{
+							Content = contentPage2,
+							Title = "Shell Tab 2",
+						}
+					}
+				}
+			);
+
+			Items.Add(tabBar);
 		}
 	}
 }
